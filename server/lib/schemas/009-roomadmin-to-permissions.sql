137c4bea07051d004fd05c54db96588515b065de
Enrique Viñé Lerma <quiquevile@gmail.com>
2026-09-22 21:14:31 +0200

    fix: remove description from migration comments to fix SQL parsing

--- server/lib/schemas/009-roomadmin-to-permissions.sql
+++ server/lib/schemas/009-roomadmin-to-permissions.sql
@@ -1,10 +1,10 @@
--- Up: convert roomadmin users to standard with all roomadmin permissions
+-- Up
 UPDATE users
 SET roleId = (SELECT roleId FROM roles WHERE name = 'standard'),
     permissions = '{"queueDelete":true,"queueMove":true,"queueReplay":true,"playerAccess":true,"playerControls":true}'
 WHERE roleId = (SELECT roleId FROM roles WHERE name = 'roomadmin');
 
--- Down: restore roomadmin role and clear permissions
+-- Down
 UPDATE users
 SET roleId = (SELECT roleId FROM roles WHERE name = 'roomadmin'),
     permissions = '{}'
